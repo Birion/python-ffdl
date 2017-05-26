e8a1158f85d132a48172a472f9843eb791795a89
Ondrej Vagner <ondrej.vagner@gmail.com>
2017-05-26 16:40:22 +0200

    Accept multiple URLs and files to update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyffdl",
-    version="0.1.3",
+    version="0.2.0",
     py_modules=["pyffdl"],
     packages=["ffdl"],
     include_package_data=True,
