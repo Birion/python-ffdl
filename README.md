62f9b40fa38335a898bdc99c64f86c827c7659c0
Ondřej Vágner <ondrej.vagner@gmail.com>
2018-09-13 14:46:52 +0200

    README cleanup

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # python-ffdl
 Fanfiction downloader
 
-[![Build Status](https://travis-ci.org/Birion/python-ffdl.svg?branch=0.7.1)](https://travis-ci.org/Birion/python-ffdl)
+[![Build Status](https://travis-ci.org/Birion/python-ffdl.svg?branch=master)](https://travis-ci.org/Birion/python-ffdl)
 
 ## Installation
 
-`$ pip install git+ssh://git@github.com/Birion/python-ffdl.git@0.7.1`
+`$ pip install git+ssh://git@github.com/Birion/python-ffdl.git@master`
 
 
 ## Usage
 
 `pyffdl.py [--update <FILE>] [<URL>[ <URL>[...]]]`
+
+## Supported sites
+
+* [adult-fanfiction.org](http://adult-fanfiction.org)
+* [archiveofourown.org](https://archiveofourown.org)
+* [fanfiction.net](https://fanfiction.net)
+* [fictionpress.com](https://fictionpress.com)
--- bin/bump-version.py
+++ bin/bump-version.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from sys import argv, exit
 
 version_file = Path("pyffdl") / "__version__.py"
-readme_file = Path("README.md")
 state_file = Path("version.current")
 
 version_pattern = re.compile(r"^VERSION = \((\d+), ?(\d+), ?(\d+)\)")
@@ -29,17 +28,12 @@ except IndexError:
 
 with version_file.open() as fp:
     version_text = fp.read()
-with readme_file.open() as fp:
-    readme_text = fp.read()
 
 try:
     old_ver = [int(x) for x in version_pattern.search(version_text).groups()]
 
     with version_file.open("w") as fp:
         fp.write(replace_version(version_text, old_ver, new_ver, ", "))
-
-    with readme_file.open("w") as fp:
-        fp.write(replace_version(readme_text, old_ver, new_ver))
 
     with state_file.open("w") as fp:
         fp.write(".".join(map(str, new_ver)))
