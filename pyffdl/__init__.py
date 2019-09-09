890506dd6eabad9fe087f88d627ecae089f6393e
Ondřej Vágner <ondrej.vagner@gmail.com>
2019-09-09 12:21:29 +0200

    Switch versions to use `versioneer`

--- pyffdl/__init__.py
+++ pyffdl/__init__.py
@@ -1,1 +1,4 @@
 from pyffdl.core import cli  # noqa: F401
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- pyffdl/__version__.py
+++
@@ -1,3 +0,0 @@
-VERSION = (0, 13, 0)
-
-__version__ = ".".join([str(x) for x in VERSION])
